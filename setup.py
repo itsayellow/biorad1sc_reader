f8f90332cced5f4ee0e8ae642a964424107ca9db
Matt Clapp <itsayellow@gmail.com>
2017-09-10 15:48:14 -0700

    from bitbucket

---
+++ setup.py
@@ -0,0 +1,40 @@
+# setup for biorad1sc_reader package
+
+import os.path
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Get the long description from the README file
+with open(os.path.join(here, 'README.rst'), 'r') as f:
+    long_description = f.read()
+
+setup(
+        name='biorad1sc_reader',
+        version='0.1',
+        description='Allows reading Bio-Rad *.1sc image/analysis files.',
+        long_description=long_description,
+        url='http://github.com/itsayellow/biorad1sc_reader/',
+        author='Matthew Clapp',
+        author_email='itsayellow+dev@gmail.com',
+        license='MIT',
+        classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Intended Audience :: Developers',
+            'Intended Audience :: Science/Research',
+            'Topic :: Multimedia :: Graphics',
+            'Topic :: Scientific/Engineering :: Bio-Informatics',
+            'Topic :: Scientific/Engineering :: Medical Science Apps.',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python :: 3'
+            ],
+        keywords='biorad 1sc biology scientific imaging',
+        packages=['biorad1sc_reader'],
+        install_requires=['Pillow'],
+        python_requires='>=3',
+        entry_points={
+            'console_scripts': [
+                'bio1sc2tiff = biorad1sc_reader.util:bio1sc2tiff',
+                ],
+            },
+        )
