9eaca9aa5c0768e97d06cb799d588cd2b5543025
Matt Clapp <itsayellow@gmail.com>
2017-10-06 13:53:07 -0700

    metadata checking implemented

--- tests/test_reader.py
+++ tests/test_reader.py
@@ -3,13 +3,14 @@
 import os
 import os.path
 import shutil
+import unittest
+import json
 from PIL import Image
 from PIL import ImageChops
 import biorad1sc_reader
-import unittest
 
 
-class TestReader(unittest.TestCase):
+class TestTiffExport(unittest.TestCase):
     tests_dir = os.path.dirname(__file__)
     testdata_dir = os.path.join(tests_dir, 'testdata')
     scratch_dir = os.path.join(tests_dir, 'scratch')
@@ -192,6 +193,63 @@ class TestReader(unittest.TestCase):
             myread.save_img_as_tiff_sc(test_img_file, invert=True)
 
             self.compare_images(ref_img_file, test_img_file)
+
+
+class BytesEncoder(json.JSONEncoder):
+    def default(self, obj):
+        # handle bytes if found
+        if isinstance(obj, bytes):
+            ret_obj = {}
+            ret_obj['__bytes__'] = True
+            ret_obj['data'] = list(obj)
+            return ret_obj
+        # for all else use default encoder
+        return json.JSONEncoder.default(self, obj)
+
+
+class TestMetadataExport(unittest.TestCase):
+    tests_dir = os.path.dirname(__file__)
+    testdata_dir = os.path.join(tests_dir, 'testdata')
+    input_files = ['test1.1sc', 'test2.1sc', 'test3.1sc', 'test4.1sc',
+            'test5.1sc']
+    meta_ref_files = ['test1_ref_meta.json', 'test2_ref_meta.json',
+            'test3_ref_meta.json', 'test4_ref_meta.json', 'test5_ref_meta.json'] 
+    meta_compact_ref_files = ['test1_ref_meta_compact.json',
+            'test2_ref_meta_compact.json', 'test3_ref_meta_compact.json',
+            'test4_ref_meta_compact.json', 'test5_ref_meta_compact.json'] 
+
+    def test_get_metadata(self):
+        for (i, infile) in enumerate(self.input_files):
+            infile_fullpath = os.path.join(self.testdata_dir, infile)
+            (inroot, _) = os.path.splitext(infile)
+
+            ref_meta_file = os.path.join(self.testdata_dir, self.meta_ref_files[i])
+
+            testread = biorad1sc_reader.Reader(infile_fullpath)
+            testmeta = testread.get_metadata()
+            testmeta_json = json.dumps(testmeta, cls=BytesEncoder, sort_keys=True)
+
+            with open(ref_meta_file, 'r') as in_fh:
+                refmeta_json = in_fh.read()
+
+            self.assertEqual(refmeta_json, testmeta_json)
+
+
+    def test_get_metadata_compact(self):
+        for (i, infile) in enumerate(self.input_files):
+            infile_fullpath = os.path.join(self.testdata_dir, infile)
+            (inroot, _) = os.path.splitext(infile)
+
+            ref_metac_file = os.path.join(self.testdata_dir, self.meta_compact_ref_files[i])
+
+            testread = biorad1sc_reader.Reader(infile_fullpath)
+            testmetac = testread.get_metadata_compact()
+            testmetac_json = json.dumps(testmetac, cls=BytesEncoder, sort_keys=True)
+
+            with open(ref_metac_file, 'r') as in_fh:
+                refmetac_json = in_fh.read()
+
+            self.assertEqual(refmetac_json, testmetac_json)
 
 
 if __name__ == '__main__':
